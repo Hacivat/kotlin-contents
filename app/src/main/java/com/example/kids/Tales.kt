78565cc38a89c407eb67efe4317930cf547c8387
Osman Pekaydın <pekaydin580@gmail.com>
2020-06-22 03:45:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kids/Tales.kt
@@ -0,0 +1,12 @@
+package com.example.kids
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Tales : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tales)
+    }
+
+}
