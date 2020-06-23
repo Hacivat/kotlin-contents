6f1f74c8154f3b1174de38d625e726071feed840
Osman Pekaydın <pekaydin580@gmail.com>
2020-06-23 04:11:03 +0300

    added drawable images and main page content loop

--- app/src/main/java/com/example/kids/MainActivity.kt
+++ app/src/main/java/com/example/kids/MainActivity.kt
@@ -1,14 +1,38 @@
 package com.example.kids
 
+import android.R
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imageView = ImageView(this)
+
+        for (i in 0..9) {
+            val image = ImageView(this)
+            imageView.layoutParams= LinearLayout.LayoutParams(400, 400)
+            imageView.x= 20F // setting margin from left
+            imageView.y= 20F // setting margin from top
+
+            val imgResId = R.drawable.group
+            var resId = imgResId
+            // Adds the view to the layout
+            imageView.setImageResource(resId)
+
+            val layout = findViewById<ScrollView>(R.id.scrollView)
+
+            layout?.addView(imageView)
+        }
     }
 
     fun openTales(view: View) {
