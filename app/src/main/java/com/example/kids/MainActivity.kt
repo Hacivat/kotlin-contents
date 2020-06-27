f53ca9da0583cd7a1144efc02eac047d2f703865
Osman Pekaydın <pekaydin580@gmail.com>
2020-06-28 01:41:32 +0300

    listed titles into saperated imageviews fuck yeah

--- app/src/main/java/com/example/kids/MainActivity.kt
+++ app/src/main/java/com/example/kids/MainActivity.kt
@@ -12,8 +12,8 @@ import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.beust.klaxon.Klaxon
 import kotlinx.android.synthetic.main.activity_main.*
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,43 +24,54 @@ class MainActivity : AppCompatActivity() {
     }
 
      private fun mapItems(titles: String) {
-        for (i in 0..9) {
-            //image config
-            val imageView = ImageView(this)
-            val lpImage = RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.MATCH_PARENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT
-            )
-            lpImage.setMargins(-25, 25, 0, 0)
-            imageView.layoutParams = lpImage
-            imageView.adjustViewBounds = true
-            val imgResId = R.drawable.item
-            imageView.setImageResource(imgResId)
+        println(parseToArray(titles))
 
-            //text config
-            val textView = TextView(this)
-            val lpText = RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT
-            )
-            lpText.setMargins(75, 120, 0, 0)
-            textView.layoutParams = lpText
-            textView.text = "Test"
-            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20F);
+        val listTitles: List<Title>? = parseToArray(titles)
 
-            //item layout config
-            val relativeLayout = RelativeLayout(this);
-            val lpRelativeLayout = RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.MATCH_PARENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT
-            )
-            relativeLayout.layoutParams = lpRelativeLayout
-            relativeLayout.addView(imageView)
-            relativeLayout.addView(textView)
+         if (listTitles != null) {
+             for (item in listTitles) {
+                 //image config
+                 val imageView = ImageView(this)
+                 val lpImage = RelativeLayout.LayoutParams(
+                     RelativeLayout.LayoutParams.MATCH_PARENT,
+                     RelativeLayout.LayoutParams.WRAP_CONTENT
+                 )
+                 lpImage.setMargins(-25, 25, 0, 0)
+                 imageView.layoutParams = lpImage
+                 imageView.adjustViewBounds = true
+                 val imgResId = R.drawable.item
+                 imageView.setImageResource(imgResId)
 
-            linearLayout?.addView(relativeLayout)
-        }
+                 //text config
+                 val textView = TextView(this)
+                 val lpText = RelativeLayout.LayoutParams(
+                     RelativeLayout.LayoutParams.WRAP_CONTENT,
+                     RelativeLayout.LayoutParams.WRAP_CONTENT
+                 )
+                 lpText.setMargins(75, 120, 0, 0)
+                 textView.layoutParams = lpText
+                 textView.text = item.title
+                 textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20F);
+
+                 //item layout config
+                 val relativeLayout = RelativeLayout(this);
+                 val lpRelativeLayout = RelativeLayout.LayoutParams(
+                     RelativeLayout.LayoutParams.MATCH_PARENT,
+                     RelativeLayout.LayoutParams.WRAP_CONTENT
+                 )
+                 relativeLayout.layoutParams = lpRelativeLayout
+                 relativeLayout.addView(imageView)
+                 relativeLayout.addView(textView)
+
+                 linearLayout?.addView(relativeLayout)
+             }
+         }
     }
+
+    private fun parseToArray(jsonStr: String): List<Title>? {
+        return Klaxon().parseArray( jsonStr )
+    }
+    data class Title(val uuid: String, val title: String)
 
     private fun getItems() {
         val url = "http:10.0.2.2:8000/api/tales/titles/tr"
@@ -68,7 +79,6 @@ class MainActivity : AppCompatActivity() {
 
         val stringRequest = StringRequest(Request.Method.GET, url,
             Response.Listener<String> { response ->
-                println(response)
                 mapItems(response)
             },
             Response.ErrorListener { error -> println(error) })
