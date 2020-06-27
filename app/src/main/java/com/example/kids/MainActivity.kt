01857ba2505acd7fe147d5548525df477018a5ea
Osman Pekaydın <pekaydin580@gmail.com>
2020-06-27 03:30:27 +0300

    Changed frame and text position for items

--- app/src/main/java/com/example/kids/MainActivity.kt
+++ app/src/main/java/com/example/kids/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.kids
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -19,34 +19,42 @@ class MainActivity : AppCompatActivity() {
         mapItems();
     }
 
-    fun mapItems() {
+     private fun mapItems() {
         for (i in 0..9) {
-            val textView = TextView(this)
+            //image config
             val imageView = ImageView(this)
-            textView.layoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-
-            val lpImage = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
+            val lpImage = RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
             )
-
             lpImage.setMargins(-25, 25, 0, 0)
             imageView.layoutParams = lpImage
             imageView.adjustViewBounds = true
-
             val imgResId = R.drawable.item
             imageView.setImageResource(imgResId)
 
-            val lpText = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
+            //text config
+            val textView = TextView(this)
+            val lpText = RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
             )
-            lpText.setMargins(0, 0, 0, 0)
+            lpText.setMargins(75, 120, 0, 0)
             textView.layoutParams = lpText
             textView.text = "Test"
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20F);
 
-            linearLayout?.addView(imageView)
-            linearLayout?.addView(textView)
+            //item layout config
+            val relativeLayout = RelativeLayout(this);
+            val lpRelativeLayout = RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+            )
+            relativeLayout.layoutParams = lpRelativeLayout
+            relativeLayout.addView(imageView)
+            relativeLayout.addView(textView)
+
+            linearLayout?.addView(relativeLayout)
         }
     }
 
